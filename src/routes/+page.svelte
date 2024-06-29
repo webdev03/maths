dbea31f53fd930b1bd353ff4072df4bf21603711
webdev03 <75148774+webdev03@users.noreply.github.com>
2024-06-29 15:00:35 +1200

    a basic landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="mx-auto flex w-full flex-col items-center gap-2 py-6 md:py-12 md:pb-8 lg:py-24 lg:pb-20 pb-8">
+	<h1
+		class="text-center text-4xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]"
+	>
+		Devarsh's Maths
+	</h1>
+	<p class="max-w-[750px] text-center text-lg sm:text-xl text-balance text-muted-foreground">
+		Learn and have fun with activities related to mathematics.
+	</p>
+</section>
