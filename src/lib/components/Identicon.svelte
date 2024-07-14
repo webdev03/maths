7b04e870f087b8493c9836e8067f664467c22327
webdev03 <75148774+webdev03@users.noreply.github.com>
2024-07-14 15:31:37 +1200

    join form

---
+++ src/lib/components/Identicon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { minidenticon } from 'minidenticons';
+
+	export let className: string;
+	export let seed: string;
+
+	$: svg = minidenticon(seed);
+</script>
+
+<div class={className}>{@html svg}</div>
--- src/routes/(mathex)/mathex/app/play/[id]/+page.svelte
+++ src/routes/(mathex)/mathex/app/play/[id]/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import Identicon from '$lib/components/Identicon.svelte';
+
 	import type { PageData } from './$types';
 	export let data: PageData;
 	const roomId = data.id;
+
+	type State = 'connecting' | 'choose-name' | 'waiting_start' | 'started' | 'finished';
+	let state: State = 'connecting';
+
+	let name: string = '';
 
 	import { io, type Socket } from 'socket.io-client';
 	import {
@@ -9,10 +16,39 @@
 		type RoomClientToServerEvents
 	} from '$lib/mathex/schemas';
 	import { toast } from 'svelte-sonner';
+	import { Input } from '$lib/components/ui/input';
+	import { Button } from '$lib/components/ui/button';
 
 	const socket: Socket<RoomServerToClientEvents, RoomClientToServerEvents> = io(`/room-${roomId}`);
 	socket.on('alert', (type, message) => {
 		// @ts-ignore
 		toast[type](message);
 	});
+	socket.on('connect', () => {
+		if (state === 'connecting') state = 'choose-name';
+		toast.success('Connected!');
+	});
+	socket.on('connect_error', () => toast.error('Failed to connect! Does this room exist?'));
+	socket.on('disconnect', () => toast.warning('Disconnected!'));
 </script>
+
+{#if state === 'connecting'}
+	<span
+		class="animate-pulse font-bold text-5xl flex text-center items-center w-full h-full justify-center"
+		>Connecting...</span
+	>
+{:else if state === 'choose-name'}
+	<div class="w-full h-full flex justify-center items-center align-middle text-center">
+		<div class="flex flex-col rounded bg-white text-slate-900 w-min text-center p-2">
+			{#if name}
+				<div class="flex justify-center text-center">
+					<Identicon seed={name} className="w-16 h-16" />
+				</div>
+			{:else}
+				<span class="w-full h-16 text-2xl text-center flex justify-center items-center">?</span>
+			{/if}
+			<Input bind:value={name} type="text" placeholder="Name" class="w-64" maxlength={20} />
+			<Button class="mt-2">Join</Button>
+		</div>
+	</div>
+{/if}
